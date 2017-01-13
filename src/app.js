764cf36c36e95c29701b9b682fbf46fe92ef5dc5
franca <francasix@outlook.com>
2017-01-13 10:11:05 +0100

    start commit

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import ReactStormpath, { Router, HomeRoute, LoginRoute, AuthenticatedRoute } from 'react-stormpath';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { IndexRoute, Route, browserHistory } from 'react-router';
+
+import { MasterPage, IndexPage, LoginPage, RegistrationPage, ProfilePage } from './pages';
+
+ReactStormpath.init();
+
+ReactDOM.render(
+  <Router history={browserHistory}>
+    <HomeRoute path='/' component={MasterPage}>
+      <IndexRoute component={IndexPage} />
+      <LoginRoute path='/login' component={LoginPage} />
+      <Route path='/register' component={RegistrationPage} />
+      <AuthenticatedRoute>
+        <HomeRoute path='/profile' component={ProfilePage} />
+      </AuthenticatedRoute>
+    </HomeRoute>
+  </Router>, document.getElementById('app-container')
+);
